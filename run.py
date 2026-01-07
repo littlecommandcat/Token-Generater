ceb101784c6a5b17f823c39271ac8ea5816ee6be
command_cat <131455122+littlecommandcat@users.noreply.github.com>
2026-01-07 10:39:37 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,17 +5,13 @@ import random, base64
 用戶id = "1234567890"  # 換成用戶id
 
 def generate_token(id:str):
-        characs = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-        characs2 = '01zr'
-        id_encoded = base64.b64encode(bytes(id, 'utf-8'))
-        random1 = f".X{random.choices(characs2, k=1)}{random.choices(characs, k=4)}.{random.choices(characs, k=27)}"
-        random2 = random1.replace("'", "")
-        random3 = random2.replace(",", "")
-        random4 = random3.replace("[", "")
-        random5 = random4.replace("]", "")
-        random_final = random5.replace(" ", "")
-        random_token = (id_encoded.decode('utf-8') + random_final).replace("=", "")
-        return random_token
+    characs = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    characs2 = '01zr'
+    id_encoded = base64.b64encode(bytes(id, 'utf-8'))
+    base_code = f".X{random.choices(characs2, k=1)}{random.choices(characs, k=4)}.{random.choices(characs, k=27)}"
+    random_code = base_code.replace("'", "").replace(",", "").replace("[", "").replace("]", "").replace(" ", "")
+    random_token = (id_encoded.decode('utf-8') + random_code).replace("=", "")
+    return random_token
 
 
 for i in range(數量):
